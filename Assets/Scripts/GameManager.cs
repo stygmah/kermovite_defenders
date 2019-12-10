b0dd4dce27363b45f40b48f22d9ef12f8784799f
guillermo bascunana soler <guillermobascunanasoler@MacBook-Pro-de-guillermo.local>
2019-12-10 22:24:14 +0100

    removed lives

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : Singleton<GameManager>
 
     [SerializeField]
     private Text moneyText;
-    [SerializeField]
-    private Text healthText;
     public int Money;
     public int Health = 5;
     public float Interest;
@@ -61,7 +59,6 @@ public class GameManager : Singleton<GameManager>
     void Start()
     {
         ChangeMoneyText(Money);
-        ChangeHealthText(Health);
         gameOverMenu.SetActive(false);
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
@@ -115,21 +112,17 @@ public class GameManager : Singleton<GameManager>
     {
         moneyText.text = "$" + quantity;
     }
-    private void ChangeHealthText(int quantity)
-    {
-        healthText.text = quantity.ToString();
-    }
 
     public void LoseLife()
     {
         Health--;
-        ChangeHealthText(Health);
     }
 
     public void GameOver()
     {
         gameOver = true;
         gameOverMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     //TOWERS
@@ -415,7 +408,6 @@ public class GameManager : Singleton<GameManager>
     }
     public void SpendOnCollect()
     {
-        //TODO: Return Kermovite to reactor
         SubtractSpecialPoint(1);
         string msg = "All the Kermovite lying around has been returned to the reactor";
         CustomMessage(msg);
