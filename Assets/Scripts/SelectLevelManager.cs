1559562d8a88560bbd1341a8ed12ea5cc7a8161c
guillermo bascunana soler <guillermobascunanasoler@MacBook-Pro-de-guillermo.local>
2019-12-22 15:53:52 +0100

    cleaned assets

--- Assets/Scripts/SelectLevelManager.cs
+++ Assets/Scripts/SelectLevelManager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SelectLevelManager : Singleton<SelectLevelManager>
 {
     public bool skipTutorial;
+
     // Start is called before the first frame update
     void Start()
     {
         skipTutorial = false;
+        DontDestroyOnLoad(transform.gameObject);
     }
 
     public void ToggleTutorial()
